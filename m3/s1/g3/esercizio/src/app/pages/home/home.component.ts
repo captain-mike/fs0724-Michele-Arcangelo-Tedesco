f658bc3d52e74a0c641079e0a87b5f9c3eea07c6
captain-mike <info@grafikweb.it>
2024-10-09 18:04:11 +0200

    esercizio

---
+++ m3/s1/g3/esercizio/src/app/pages/home/home.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { iJSONresponse } from '../../Model/jsonresponse';
+import { iPost } from '../../Model/i-post';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss'
+})
+export class HomeComponent implements OnInit {
+
+  featuredPost!:iPost//post in evidenza
+  postArr:iPost[] = []//tutti i post
+
+  ngOnInit(): void {
+
+    fetch('/assets/db.json')
+    .then(res => <Promise<iJSONresponse>> res.json())
+    .then(res => {
+
+      this.featuredPost = res.posts[0]
+
+      this.postArr = res.posts.slice(0,4)
+
+    })
+
+  }
+
+
+
+}
